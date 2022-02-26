d994ddb6a3fb5e1d63b9bd3e46c4107c19c08110
juris <spider2005@inbox.lv>
2022-02-26 14:31:35 +0200

    saglabāts2

--- darbs26.02.py
+++ darbs26.02.py
@@ -1,8 +1,5 @@
 # 2.uzdevums
-agent=(input("Lūdzu ievadiet savu vārdu:"))
-if agent==Bond:
-    print("Esi sveicināts 007")
-else:
-    print("Esi sveicināts", agent)
+agent= str(input("Kā Jūs sauc:"))
+
 
 
