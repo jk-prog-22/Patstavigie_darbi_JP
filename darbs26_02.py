f4058df788d55522223644c7fe1716f780729968
juris <spider2005@inbox.lv>
2022-03-03 20:10:20 +0200

    2.uzdevums

--- darbs26_02.py
+++ darbs26_02.py
@@ -1,16 +1,23 @@
 # 2.uzdevums
 
+
 agent= str(input("Kā Jūs sauc:"))
 agent2="bond"
-if agent=="Bond":
+
+if agent=="bond" and "Bond":
     print("Esi sveicināts 007")
 else:
     print("Esi sveicināts", agent)
 
 
+
+
+
 #3.uzdevums
 
 number=int(input("ievadi skaitli:"))
-
-if number!=
+if number % 2==0:
+    print("Vesels skaitlis")
+else:
+    print("nepāra skaitlis")
 
